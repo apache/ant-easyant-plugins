d6e8d681373455a79f07552f2bfef133b5b5ac21
Jean-Louis Boudart <jlboudart@apache.org>
2012-08-04 21:21:06 +0000

    Add junit sample for tests
    git-svn-id: https://svn.apache.org/repos/asf/incubator/easyant/plugins/trunk@1369460 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test-junit/src/test/antunit/src/integration-test/java/org/apache/easyant/SampleITTest.java
@@ -0,0 +1,13 @@
+package org.apache.easyant;
+
+import junit.framework.TestCase;
+
+public class SampleITTest extends TestCase {
+    public void testTrue() {
+       assertTrue(true);
+    }
+    public void testShouldFail() {
+       assertTrue(false);
+    }
+
+}
---
+++ test-junit/src/test/antunit/src/test/java/org/apache/easyant/SampleTest.java
@@ -0,0 +1,13 @@
+package org.apache.easyant;
+
+import junit.framework.TestCase;
+
+public class SampleTest extends TestCase {
+    public void testTrue() {
+       assertTrue(true);
+    }
+    public void testShouldFail() {
+       assertTrue(false);
+    }
+
+}
